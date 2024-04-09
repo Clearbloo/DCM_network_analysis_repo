9469c8ca7edf7759a499e1993762e0f7c7c3548a
Clearbloo <68247573+Clearbloo@users.noreply.github.com>
2024-04-09 23:45:38 +0100

    attempting to add a paper extractor

--- src/constants.py
+++ src/constants.py
@@ -3,3 +3,4 @@ import os.path as osp
 CWD = pathlib.Path(__file__).parent.parent.resolve()
 DATA_DIR = osp.join(CWD, "data")
 OUTPUT_DIR = osp.join(CWD, "results")
+DEFAULT_MODEL_DIR = osp.join(OUTPUT_DIR, "model_20240131_225522")
